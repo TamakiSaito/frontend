ac054bee06505b0945397209ecec0a264d7ef8d2
TamakiSaito <immacd1012@gmail.com>
2024-05-14 21:30:52 +0900

    feat: vue ref reactive study

---
+++ 5-vue/1-vue3/6-ref-reactive/app1/src/components/Movies.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <h1>Movies</h1>
+  <ul>
+    <li v-for="movie in movies" :key="movie.title">
+      {{  movie.title }} ({{ movie.year }})
+    </li>
+  </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+// ref -> number, string, boolean, null, object
+// reactive -> array
+
+const movies = reactive([
+  { title: 'Movie 1', year: 2021 },
+  { title: 'Movie 2', year: 2022 },
+  { title: 'Movie 3', year: 2023 },
+  { title: 'Movie 4', year: 2024 },
+  { title: 'Movie 5', year: 2025 },
+]) 
+
+movies.push({ title: 'Movie 6', year: 2026 })
+</script>
+
+<style scoped>
+</style>
