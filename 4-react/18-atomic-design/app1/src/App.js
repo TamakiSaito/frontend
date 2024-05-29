20270039df6b901bbe13d69376de83ed9b7ff06f
TamakiSaito <immacd1012@gmail.com>
2024-05-29 21:32:54 +0900

    custom-hooks

---
+++ 4-react/18-atomic-design/app1/src/App.js
@@ -0,0 +1,80 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Movies from "./pages/Movies";
+import Count from "./pages/Count";
+import News from "./pages/News";
+import Todos from "./pages/Todos";
+import Home from "./pages/Home";
+// import { Routes, Route, Link } from "react-router-dom";
+// import Movies from "./components/Movies";
+// import Count from "./components/Count";
+// import News from "./components/News";
+// import Todos from "./components/Todos";
+
+// export default function App() {
+//   return (
+//     <div className="m-[20px]">
+//       <span>
+//         <Link to="/">Home</Link>
+//       </span>
+//       <span>
+//         <Link to="/movie">Movies</Link>
+//       </span>
+//       <span>
+//         <Link to="/count">Count</Link>
+//       </span>
+//       <span>
+//         <Link to="/news">News</Link>
+//       </span>
+//       <span>
+//         <Link to="/todos">Todos</Link>
+//       </span>
+
+//       <hr />
+
+export default function App() {
+  return (
+    <Router>
+      <div className="m-[20px]">
+        <span>
+          <Link to="/">Home</Link> /
+        </span>
+        <span>
+          <Link to="/movie">Movies</Link> /
+        </span>
+        <span>
+          <Link to="/count">Count</Link> /
+        </span>
+        <span>
+          <Link to="/news">News</Link> /
+        </span>
+        <span>
+          <Link to="/todos">Todos</Link>
+        </span>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie" element={<Movies />} />
+          <Route path="/count" element={<Count />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/todos" element={<Todos />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={<h1 className="text-[36px] font-bold">Home</h1>}
+//         />
+//         <Route path="/movie" element={<Movies />} />
+//         <Route path="/count" element={<Count />} />
+//         <Route path="/news" element={<News />} />
+//         <Route path="/todos" element={<Todos />} />
+//       </Routes>
+//     </div>
+//   );
+// }
