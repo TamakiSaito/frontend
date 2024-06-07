16bea0b702db95289f9cc38a085a3a842f9f0e4e
TamakiSaito <immacd1012@gmail.com>
2024-06-07 21:46:20 +0900

    JS study

--- 3-js/0-JavaScript-study/app/index.js
+++ 3-js/0-JavaScript-study/app/index.js
@@ -2,20 +2,49 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => {
+    add(todo);
+  });
+}
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   add();
 });
 
-function add() {
+function add(todo) {
   let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
   if (todoText) {
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
+
+    if (todo && todo.completed) {
+      li.classList.add("text-decoration-line-through");
+    }
+
+    li.addEventListener("contextmenu", function (event) {
+      event.preventDefault();
+      li.remove();
+      saveData();
+    });
+
+    li.addEventListener("click", function () {
+      li.classList.toggle("text-decoration-line-through");
+      saveData();
+    });
+
     ul.appendChild(li);
     input.value = "";
-    savaData();
+    saveData();
   }
 }
 
@@ -23,6 +52,11 @@ function saveData() {
   const lists = document.querySelectorAll("li");
   let todos = [];
   lists.forEach((list) => {
-    todos.push(list.innerText);
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains("text-decoration-line-through"),
+    };
+    todos.push(todo);
   });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
