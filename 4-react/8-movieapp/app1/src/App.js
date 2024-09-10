ea31cf052435003269e7b9e01a1beabe5b417703
TamakiSaito <immacd1012@gmail.com>
2024-09-10 22:52:17 +0900

    feat: 19 study4

--- 4-react/8-movieapp/app1/src/App.js
+++ 4-react/8-movieapp/app1/src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   const [isMoviesOpen, setIsMoviesOpen] = useState(false);
   return (
     <div className="App">
+      {/* Moviesをクリックしたら{isMoviesOpen} <Movies />を出す */}
       <button onClick={() =>setIsMoviesOpen(!isMoviesOpen)}>Movies</button>
       <div>{isMoviesOpen && <Movies />}</div>
     </div>
--- 4-react/8-movieapp/app1/src/components/Movies.js
+++ 4-react/8-movieapp/app1/src/components/Movies.js
@@ -8,6 +8,7 @@ export default function Movies() {
   // mounted
   useEffect(() => {
     fetch("https://yts.mx/api/v2/list_movies.json")
+    // responseをもらって、json化して、json化したものがmoviesに入る
     .then((res) => res.json())
     .then((json) => { 
       setMovies(json.data.movies);
@@ -16,6 +17,7 @@ export default function Movies() {
 
   console.log("movies: ", movies);
 
+  // moviesの中のtitleをmapで取得
   const render = movies.map(movie => {
     return (
       <div key={movie.id}>
