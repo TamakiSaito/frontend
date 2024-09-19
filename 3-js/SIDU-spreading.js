5081f8ae8612ef1834a5d29d1280f2eabdaaf8a2
TamakiSaito <immacd1012@gmail.com>
2024-09-19 21:06:56 +0900

    feat: 23 study

--- 3-js/SIDU-spreading.js
+++ 3-js/SIDU-spreading.js
@@ -1,6 +1,5 @@
 // 不変性の法則
 // Reactは不変性を守らないといけない
-//
 
 // SIDU
 
@@ -12,12 +11,15 @@ const addArr = [...arr, 6];
 console.log(addArr); // [1, 2, 3, 4, 5, 6]
 
 // Delete
+// indexの配列の一番最後を削除(arr.length -1)
 const delArr = arr.filter((item, index, arr) => {
   return index !== arr.length -1;
 });
 console.log(delArr);
 
 // Update
+// modify : 修正
+// indexの配列の一番最後を999にして他はそのまま
 const modArr = arr.map((item, index, arr) => {
   return index === arr.length - 1 ? 999 : item;
 });
--- 3-js/shallow-deep.js
+++ 3-js/shallow-deep.js
@@ -1,10 +1,10 @@
-//DEEP?
+//DEEP：Primitive Tyope / 値参照タイプ(数字・文字列・Boolean)
 let a = 1;
 let b = a;
 a = 2;
 console.log(b); // 1
 
-// Shallow Copy
+// Shallow Copy : Referencial Type / 住所参照タイプ(配列・オブジェクト・関数)
 const c = [1, 2, 3];
 const d = c; // cの住所を代入する
 c[0] = 999;
@@ -13,10 +13,17 @@ console.log(d); // [999, 2, 3];
 // cが変わってもdが変わらないようにコピーしたいならどうすればいい？
 // Deep Copy
 const cc = [1, 2, 3];
+// JSONは内蔵クラス
+// JSON.stringify = 文字列にする
+// parse = パッシング(PCがわかるようにする) "[1, 2, 3]"を[1, 2, 3]へと変換する
 // const dd = JSON.parse(JSON.stringify(cc)); // "[1, 2, 3]"の値を持つ新しい住所を代入する
+
+// Object.assign([], cc)がよりいい
 const dd = Object.assign([], cc);
 cc[0] = 999; // ddとは関係ない
 console.log(dd); // [1, 2, 3]
+
+
 
 // Shallow Copy
 // const a = { id: "a123", pw: "asdfghjkl" };
@@ -30,6 +37,8 @@ const bb = Object.assign({}, aa); // const bb = JSON.parse(JSON.stringify(aa));
 aa.id = "a999";
 console.log(bb.id); // a123
 
+
+
 const arr1 = [1, 2, 3];
 const obj1 = { id: "a123", pw: "asdfghjkl" };
 // Shallow Copy
@@ -41,6 +50,8 @@ const obj1 = { id: "a123", pw: "asdfghjkl" };
 // console.log(arr2); // [999, 2, 3]
 // console.log(obj2); // { id: "a999", pw: "asdfghjkl"}
 
+
+// ...を使用する方がベスト
 // Deep Copy : Spreading文法
 const arr3 = [...arr1];
 const obj3 = { ...obj1 };
