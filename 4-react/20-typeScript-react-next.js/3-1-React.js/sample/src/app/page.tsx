519cc9f6bcf554d10b5ca40c1cdd302ab532189a
TamakiSaito <immacd1012@gmail.com>
2025-01-02 23:05:04 +0900

    React Component

--- 4-react/20-typeScript-react-next.js/3-1-React.js/sample/src/app/page.tsx
+++ 4-react/20-typeScript-react-next.js/3-1-React.js/sample/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hello from "../components/Hello";
+import Counter from "../components/Counter";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
       <Hello />
+      <Counter initialValue={0}/>
     </div>
   );
 }
