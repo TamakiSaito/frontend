ec279e8109a953807c4b8f05516556c0345e05c9
TamakiSaito <immacd1012@gmail.com>
2025-01-14 22:21:36 +0900

    text 159-161

--- 4-react/20-typeScript-react-next.js/3-2-Next.js/next-sample/src/app/components/styledButton/styleButton.stories.tsx
+++ 4-react/20-typeScript-react-next.js/3-2-Next.js/next-sample/src/app/components/styledButton/styleButton.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { StyledButton } from './index'
 
@@ -8,8 +9,17 @@ const meta: Meta<typeof StyledButton> = {
     variant: {
       control: { type: 'select', options: ['primary', 'success', 'transparent'] },
     },
+    onClick: {
+      action: 'clicked'
+    }
   },
 }
+
+
+type PrimaryProps = {
+  incrementAction: (e: React.MouseEvent, count: number) => void;
+};
+
 
 export default meta
 type Story = StoryObj<typeof StyledButton>
@@ -18,6 +28,22 @@ export const Primary: Story = {
   args: {
     variant: 'primary',
     children: 'Primary Button',
+    onClick: () => console.log('Button clicked'),
+  },
+  render: (args) => {
+    const [count, setCount] = useState(0);
+    const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (args.onClick) {
+        args.onClick(e);
+      }
+      setCount((c) => c + 1);
+    };
+
+    return (
+      <StyledButton {...args} variant="primary" onClick={onClick}>
+        Count: {count}
+      </StyledButton>
+    )
   },
 }
 
