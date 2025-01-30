f1436bdb48f34f77c3365c0781efc45eea13aaf2
TamakiSaito <immacd1012@gmail.com>
2025-01-30 22:41:00 +0900

    text 168-169

--- 4-react/20-typeScript-react-next.js/app1/src/components/Button.test.js
+++ 4-react/20-typeScript-react-next.js/app1/src/components/Button.test.js
@@ -3,6 +3,6 @@ import { render, screen } from "@testing-library/react";
 import Button from "./Button";
 
 test("ボタンが表示される", () => {
-  render(<Button>クリック</Button>);
-  expect(screen.getByText("クリック")).toBeInTheDocument();
+  render(<Button>Click</Button>);
+  expect(screen.getByText("Click")).toBeInTheDocument();
 });
